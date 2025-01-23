8d17768b06f2db39fc462161b84fc887dfc20334
Baasandash0307 <bsskaa.kai123@gmail.com>
2025-01-23 12:16:55 +0800

    new

--- calc.js
+++ calc.js
@@ -1,4 +1,29 @@
 
 
+const tipsRef = document.getElementById('perc');
+const tips = document.getElementsByClassName('tip-buttons');
+
+for (let i = 0; i < tips.length; i++){
+    tips[i].addEventListener('click', (event) => {
+        const tipStrings = event.target.innerHTML;
+        const tipsGetValues = parseFloat(tipStrings.replace('%', ''));
+        const input = document.getElementById('billAmount')
+        const inputValue = parseFloat(input.value);
+        
+        if(!isNaN(inputValue)) {
+            const result = (inputValue * tipsGetValues) / 100; // 10 
+            const sum = result + inputValue;
+            document.getElementById('totalTip').innerHTML = sum;  
+        }
+        else{
+            console.log('error');
+        }
+    })
+};
+    
+    document.getElementById('clear').addEventListener('click', () => {
+    document.getElementById('billAmount').value = '';
+    document.getElementById('tip').textContent = '';
+});
 
 
