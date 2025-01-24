e203753e0f16f1ff679a44574bcacbb669ca3a8b
Baasandash0307 <bsskaa.kai123@gmail.com>
2025-01-24 09:22:38 +0800

    new

--- calc.js
+++ calc.js
@@ -11,9 +11,9 @@ for (let i = 0; i < tips.length; i++){
         const inputValue = parseFloat(input.value);
         
         if(!isNaN(inputValue)) {
-            const result = (inputValue * tipsGetValues) / 100; // 10 
+            const result = (inputValue * tipsGetValues) / 100;
             const sum = result + inputValue;
-            document.getElementById('totalTip').innerHTML = sum;  
+            document.getElementById('tip').innerHTML = sum;  
         }
         else{
             console.log('error');
